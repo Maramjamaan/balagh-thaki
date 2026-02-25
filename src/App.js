8a40edecbffe4deda2ce746d37efb6d6cef3677b
Maram Alzahrani <maramjamaan2@gamil.com>
2026-02-25 22:07:00 +0300

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React from 'react';
-import { Router } from 'react-router-dom';
-
-import GlobalStyles from './assets/styles/global';
-import Routes from './routes';
-import history from './routes/history';
-
-export default function App() {
+function App() {
   return (
-    <Router history={history}>
-      <GlobalStyles />
-
-      <Routes />
-    </Router>
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>بلاغ ذكي </h1>
+      <p>المشروع شغال!</p>
+    </div>
   );
 }
+
+export default App;
